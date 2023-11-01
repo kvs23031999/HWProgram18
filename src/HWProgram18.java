1328ea80ebf7c83619f9c32ee573a5f667382943
Vlad <147541054+kvs23031999@users.noreply.github.com>
2023-11-01 17:12:33 +0200

    first commit

---
+++ src/HWProgram18.java
@@ -0,0 +1,70 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class HWProgram18 {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[8][8];
+        int[] matrix1 = new int[8];
+        for (int i = 0; i < matrix1.length; i++) {
+            matrix1[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(matrix1));
+
+        int[] matrix2 = new int[8];
+        for (int i = 0; i < matrix2.length; i++) {
+            matrix2[i] = 8 - i;
+        }
+        System.out.println(Arrays.toString(matrix2));
+
+        int[] matrix3 = new int[8];
+        for (int i = 0; i < matrix3.length; i++) {
+            matrix3[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(matrix3));
+
+        int[] matrix4 = new int[8];
+        for (int i = 0; i < matrix4.length; i++) {
+            matrix4[i] = 8 - i;
+        }
+        System.out.println(Arrays.toString(matrix4));
+
+        int[] matrix5 = new int[8];
+        for (int i = 0; i < matrix5.length; i++) {
+            matrix5[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(matrix5));
+
+        int[] matrix6 = new int[8];
+        for (int i = 0; i < matrix6.length; i++) {
+            matrix6[i] = 8 - i;
+        }
+        System.out.println(Arrays.toString(matrix6));
+
+        int[] matrix7 = new int[8];
+        for (int i = 0; i < matrix7.length; i++) {
+            matrix7[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(matrix7));
+
+        int[] matrix8 = new int[8];
+        for (int i = 0; i < matrix8.length; i++) {
+            matrix8[i] = 8 - i;
+        }
+        System.out.println(Arrays.toString(matrix8));
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
